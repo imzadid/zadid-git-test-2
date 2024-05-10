10cc8b27b271be8f76761f69ddbe511082cf4f82
imzadid <muhammadzadid061512@gmail.com>
2024-05-10 21:00:57 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui doloremque consequuntur nobis neque repudiandae quas illum dignissimos totam sit ab asperiores, iusto explicabo consequatur ipsum eaque omnis doloribus maxime eos?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
